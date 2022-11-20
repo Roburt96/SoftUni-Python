e9790d7738fa9dd699b90c582d76d0453c1dbdc8
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2022-11-20 17:44:41 +0200

    class prog

--- Python_Advance/03_Tuples_and_Sets_Lab/04_Parking_Lot.py
+++ Python_Advance/03_Tuples_and_Sets_Lab/04_Parking_Lot.py
@@ -1,16 +1,57 @@
-car_entry = set()
+# car_entry = set()
+#
+# number_of_commands = int(input())
+# for i in range(number_of_commands):
+#     cmd, car_plate = input().split(", ")
+#     if 'IN' in cmd:
+#         car_entry.add(car_plate)
+#     elif 'OUT' in cmd:
+#         car_entry.remove(car_plate)
+#
+#
+# if car_entry:
+#     print("\n".join(car_entry))
+# else:
+#     print(f"Parking Lot is Empty")
 
-number_of_commands = int(input())
-for i in range(number_of_commands):
-    cmd, car_plate = input().split(", ")
-    if 'IN' in cmd:
-        car_entry.add(car_plate)
-    elif 'OUT' in cmd:
-        car_entry.remove(car_plate)
+
+class Parking:
+
+    def __init__(self):
+        self.parking = set()
+
+    def in_parking(self, plate):
+        self.parking.add(plate)
+
+    def out_parking(self, plate):
+        self.parking.remove(plate)
 
 
-if car_entry:
-    print("\n".join(car_entry))
-else:
-    print(f"Parking Lot is Empty")
+all_car = Parking()
+
+
+def main():
+    number_of_commands = int(input())
+    for i in range(number_of_commands):
+        cmd, car_plate = input().split(", ")
+        if 'IN' in cmd:
+            all_car.in_parking(car_plate)
+        elif 'OUT' in cmd:
+            all_car.out_parking(car_plate)
+
+    if all_car.parking:
+        print(Parking)
+    else:
+        print(f"Parking Lot is Empty")
+
+
+main()
+
+
+
+
+
+
+
+
 
