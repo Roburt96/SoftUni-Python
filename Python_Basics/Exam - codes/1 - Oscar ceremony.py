56ab8182b567415dfc02b7294dcacbf78460bbb4
Roburt Syulev <110181057+Roburt96@users.noreply.github.com>
2022-11-23 10:58:15 +0200

    Delete Python_Basics/Exam - codes directory

--- Python_Basics/Exam - codes/1 - Oscar ceremony.py
+++
@@ -1,8 +0,0 @@
-rent = int(input())
-
-statuette = rent - ( rent * 0.30)
-catering = statuette - (statuette * 0.15)
-music = catering / 2
-
-total_sum = rent + statuette + catering + music
-print(f"{total_sum:.2f}")
