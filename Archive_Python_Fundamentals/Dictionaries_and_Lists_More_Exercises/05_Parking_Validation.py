c4cd31f1c1edf786a601dd65441f48b2f909235a
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2022-11-25 13:48:59 +0200

    90/100

--- Archive_Python_Fundamentals/Dictionaries_and_Lists_More_Exercises/05_Parking_Validation.py
+++ Archive_Python_Fundamentals/Dictionaries_and_Lists_More_Exercises/05_Parking_Validation.py
@@ -2,14 +2,20 @@ import re
 plates = {}
 
 
-def register_(name, plate):
+def register_(name, plate, valid):
     if name in plates:
         print(f"ERROR: already registered with plate number {plate}")
 
-        # print(f"ERROR: license plate {plate} is busy")
-    else:
-        plates[name] = plates.get(name, {'plate': plate})
-        print(f"{name} registered {plate} successfully")
+    elif not valid:
+        print(f"ERROR: invalid license plate {plate}")
+
+    elif valid:
+        if any([True if plates[key] == plate else False for key in plates]):
+            print(f"ERROR: license plate {plate} is busy")
+
+        else:
+            plates[name] = plate
+            print(f"{name} registered {plate} successfully")
 
 
 def unregister_(name):
@@ -20,22 +26,20 @@ def unregister_(name):
         del plates[name]
 
 
-
 num_of_plates = int(input())
 for i in range(num_of_plates):
     cmd, *info = [x for x in input().split()]
 
-    if 'register' in cmd:
+    if cmd == 'register':
         name = info[0]
         plate = info[1]
-        pattern = (r"(?P<check>[A-Z]{2}[0-9]{4}[A-Z]{2})")
-        check_valid = re.finditer(pattern, plate)
+        pattern = (r"\b([A-Z]{2}[0-9]{4}[A-Z]{2})\b")
+        check_valid = re.match(pattern, plate)
+        register_(name, plate, check_valid)
 
-        if check_valid and len(plate) == 8:
-            register_(name, plate)
-        else:
-            print(f"ERROR: invalid license plate {plate}")
-            pass
-    elif 'unregister' in cmd:
+    elif cmd == 'unregister':
         name = info[0]
         unregister_(name)
+
+for key, value in plates.items():
+    print(f"{key} => {value}")
