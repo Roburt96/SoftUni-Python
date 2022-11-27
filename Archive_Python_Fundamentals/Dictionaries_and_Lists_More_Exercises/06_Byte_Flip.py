b8768353d314b4d4daa280466c42094b38e8aef2
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2022-11-27 08:53:55 +0200

    100/100

--- Archive_Python_Fundamentals/Dictionaries_and_Lists_More_Exercises/06_Byte_Flip.py
+++ Archive_Python_Fundamentals/Dictionaries_and_Lists_More_Exercises/06_Byte_Flip.py
@@ -1,33 +1,24 @@
 
-# strings = [x for x in input().split()]
-#
-# for ch in strings:
-#     if len(ch) != 2:
-#         strings.remove(ch)
-# print(strings)
-# strings.reverse()
-# print(strings)
-#
-# new_list = []
-# for i in range(len(strings)):
-#     x = hex(int(strings[i]))
-#     new_list.append(x)
-# print(new_list)
+strings = [x for x in input().split()[::-1]]
+new_strings = []
+for ch in strings:
+    if len(ch) != 2:
+        strings.remove(ch)
+
+for i in strings:
+    for num in range(len(i) - 1):
+        second_num = i[num]
+        first_num = i[-1]
+        new_num = first_num + second_num
+        new_strings.append(new_num)
+
+s = ''.join(new_strings)
+print(s)
+print(bytes.fromhex(s).decode('utf-8'))
 
 
-# num = ['21', '23', '44', '55', '22', '6F']
-# s = [','.join('{0:.2f}'.format(x) for x in num)]
-# print(s)
 
-# new_strings = []
-#
-# for i in strings:
-#     for num in range(len(i) - 1):
-#         second_num = i[num]
-#         first_num = i[-1]
-#         new_num = first_num + second_num
-#         new_strings.append(new_num)
+# x = ''.join(strings)
+# s = str(x)
+# print(bytes.fromhex(s).decode('utf-8'))
 
-# num = '6F'
-# h1 = hex(num)
-# print("The ", num, " in hexadecimal is: ", h1[2:])
