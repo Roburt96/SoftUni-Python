8ee62fe50a264468e7dee5c62c2498eea51e7617
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2022-12-11 20:22:30 +0200

    proc

---
+++ Python_Advance_Exams/05_Python Advanced Exam - 19 February 2022/01_Flowers_Findeers.py
@@ -0,0 +1,14 @@
+from collections import deque
+vowels = deque(input().split())
+consonants = deque(input().split())
+find_flowers = {
+    "rose": "rose",
+    "tulip": "tulip",
+    "lotus": "lotus",
+    "daffodil": "daffodil"
+}
+
+find = False
+
+while vowels and consonants and not find:
+    pass
