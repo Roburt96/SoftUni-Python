5ccdccfc5f0252e280d53c907e0d4984e8126e63
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2022-12-11 10:07:39 +0200

    100/100

---
+++ Python_Advance_Exams/Final_Exams/Python Advanced Retake Exam - 18 August 2022/01_Stewards.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+seats = input().split(", ")
+row_one_numbers = deque(int(x) for x in input().split(', '))
+row_two_numbers = deque(int(x) for x in input().split(', '))
+counter, find_seats = 0, []
+
+while len(find_seats) != 3 and counter != 10:
+
+    num_row_one = row_one_numbers.popleft()
+    num_row_two = row_two_numbers.pop()
+    counter += 1
+    letter_seat = chr(num_row_one + num_row_two)
+
+    find = False
+    for seat in (f"{num_row_one}{letter_seat}", f"{num_row_two}{letter_seat}"):
+        if seat in seats:
+            find_seats.append(seat)
+            seats.remove(seat)
+            find = True
+
+    if find:
+        continue
+
+    row_one_numbers.append(num_row_one)
+    row_two_numbers.appendleft(num_row_two)
+
+print(f"Seat matches: {', '.join(find_seats)}")
+print(f"Rotations count: {counter}")
+
