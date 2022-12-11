989fdfefdd292c61b5e525867c2c5892894aff19
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2022-12-11 08:42:11 +0200

    done

---
+++ Python_Advance/10_Functions_Advanced_Exercise/09_Recursion_Power.py
@@ -0,0 +1,20 @@
+def palindrome(word, inx):
+    if word == word[::-1]:
+        return f"{word} is polindrome"
+    return f"{word} is not a polindrome"
+
+# def palindrome(word, idx):
+#     if idx == len(word) // 2:
+#         return f"{word} is a palindrome"
+#     if word[idx] == word[-idx-1]:
+#         print(word[-idx-1])
+#         return palindrome(word, idx+1)
+#     else:
+#         return f"{word} is not a palindrome"
+
+
+
+
+
+print(palindrome("abcba", 0))
+print(palindrome("peter", 0))
