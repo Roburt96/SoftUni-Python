cd10640ff0f6585a8f9bc25fa10915da96d32fb4
Roburt96 <roburtsulev96@gmail.com>
2022-12-18 15:17:47 +0200

    92/100

---
+++ Python_Advance_Exams/10_Python Advanced Retake Exam - 16 December 2020/01_Mathing_Problem.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+
+males = deque(int(x) for x in input().split())     # -1
+females = deque(int(x) for x in input().split())   # 1
+matches = 0
+
+
+while males and females:
+    if males[-1] <= 0:
+        males.pop()
+        continue
+    if females[0] <= 0:
+        females.popleft()
+        continue
+    if males[-1] % 25 == 0:
+        males.pop()
+        males.pop()
+        continue
+    if females[0] % 25 == 0:
+        females.popleft()
+        females.popleft()
+        continue
+
+    current_male = males.pop()
+    current_female = females.popleft()
+
+    if current_male == current_female:
+        matches += 1
+
+
+    elif current_female < current_male:
+        current_male -= 2
+        males.append(current_male)
+
+print(f"Matches: {matches}")
+if not males:
+    print("Males left: none")
+elif males:
+    print(f"Males left: {', '.join(str(x) for x in reversed(males))}")
+if not females:
+    print("Females left: none")
+elif females:
+    print(f"Females left: {', '.join(str(x) for x in females)}")
