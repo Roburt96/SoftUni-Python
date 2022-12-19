f0682d3ccaa00181f9b3f9fe34a7fe7784566565
Roburt96 <roburtsulev96@gmail.com>
2022-12-19 17:09:27 +0200

    100/100

--- Python_Advance_Exams/12_Python Advanced Retake Exam - 19 August 2020/01_Taxi_Express.py
+++ Python_Advance_Exams/12_Python Advanced Retake Exam - 19 August 2020/01_Taxi_Express.py
@@ -1,5 +1,24 @@
 from collections import deque
 
-customers = deque(int(x) for x in input().split(', '))
-taxis = deque(int(x) for x in input().split(', '))
+customers = deque(int(x) for x in input().split(', '))     # popleft
+taxis = deque(int(x) for x in input().split(', '))         # pop
+total_time = 0
 
+while customers and taxis:
+    current_taxi = taxis.pop()
+    current_customer = customers.popleft()
+
+    if current_taxi >= current_customer:
+        total_time += current_customer
+
+    else:
+        customers.appendleft(current_customer)
+
+
+if not customers:
+    print("All customers were driven to their destinations")
+    print(f"Total time: {total_time} minutes")
+
+else:
+    print("Not all customers were driven to their destinations")
+    print(f"Customers left: {', '.join(str(x) for x in customers)}")
