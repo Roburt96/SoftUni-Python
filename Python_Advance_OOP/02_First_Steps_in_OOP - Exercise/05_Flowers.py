0b9e16d68cb5dadd626cd181d3bc770691b1f66a
Roburt96 <roburtsulev96@gmail.com>
2022-12-22 17:26:10 +0200

    100/100

---
+++ Python_Advance_OOP/02_First_Steps_in_OOP - Exercise/05_Flowers.py
@@ -0,0 +1,25 @@
+class Flower:
+    is_happy = False
+
+    def __init__(self, name: str, water_requirements: int):
+
+        self.name = name
+        self.water_requirements = water_requirements
+
+
+    def water(self, quantity):
+        if quantity >= self.water_requirements:
+            Flower.is_happy = True
+
+    def status(self):
+        if Flower.is_happy:
+            return f"{self.name} is happy"
+        return f"{self.name} is not happy"
+
+flower = Flower("Lilly", 100)
+flower.water(50)
+print(flower.status())
+flower.water(60)
+print(flower.status())
+flower.water(100)
+print(flower.status())
