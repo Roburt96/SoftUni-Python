3297ff5943b493e4076d22ecf05506e74f08cbcf
Roburt96 <roburtsulev96@gmail.com>
2022-12-24 16:40:22 +0200

    .

--- Python_Advance_Exams/05_Python Advanced Exam - 19 February 2022/01_Flowers_Findeers.py
+++ Python_Advance_Exams/05_Python Advanced Exam - 19 February 2022/01_Flowers_Findeers.py
@@ -1,7 +1,7 @@
 from collections import deque
 vowels = deque(input().split())
 consonants = deque(input().split())
-find_flowers = {
+flowers = {
     "rose": "rose",
     "tulip": "tulip",
     "lotus": "lotus",
@@ -9,6 +9,26 @@ find_flowers = {
 }
 
 find = False
+word = ""
 
 while vowels and consonants and not find:
-    pass
+    first_ch = vowels.popleft()
+    second_ch = consonants.pop()
+    for key, value in flowers.items():
+        if first_ch in value:
+            flowers[key] = flowers[key].replace(first_ch, "")
+        if second_ch in value:
+            flowers[key] = flowers[key].replace(second_ch, "")
+        if not flowers[key]:
+            word = key
+            find = True
+            break
+
+if find:
+    print(f"Word found: {word}")
+elif not find:
+    print("Cannot find any word!")
+if vowels:
+    print(f"Vowels left: {' '.join(vowels)}")
+if consonants:
+    print(f"Consonants left: {' '.join(consonants)}")
