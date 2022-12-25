3be9e159bdc6d9d64c0d8fb5d88df56a4be83862
Roburt96 <roburtsulev96@gmail.com>
2022-12-25 20:26:44 +0200

    100/100

--- Python_Advance_Exams/06_Python Advanced Retake Exam - 15 December 2021/03_Naughty_or_Nice.py
+++ Python_Advance_Exams/06_Python Advanced Retake Exam - 15 December 2021/03_Naughty_or_Nice.py
@@ -1,26 +1,93 @@
 def naughty_or_nice_list(*args, **kwargs):
-    santa_dict ={
+    santa_dict = {
         "Nice": [],
         "Naughty": [],
         "Not found": []
     }
-    santa_list = []
+    santa_list = args[0]
+    pass
 
-    for num, kid in args:
-        num = args[0]
-        kid = args[1]
+    for i in args[1:]:
+        num, cmd = [int(x) if x.isdigit() else x for x in i.split('-')]
+        counter = 0
+        name = ""
+        current = ''
+        for check in santa_list:
+            if num == check[0]:
+                counter += 1
+                current = check
+                name = check[1]
+        if counter == 1:
+            santa_dict[cmd].append(name)
+            santa_list.remove(current)
 
-    santa_list = sorted(santa_list)
+    for key, cmd_value in kwargs.items():
+        counter_name = 0
+        current_kwargs = ""
+        current = ""
+        for pos, (current_num, current_name) in enumerate(santa_list):
+            if key == current_name:
+                counter_name += 1
+                current_kwargs = current_name
+                current = pos
+        if counter_name == 1:
+            santa_list.pop(current)
+            santa_dict[cmd_value].append(current_kwargs)
+
+
+    for left_kid in santa_list:
+        santa_dict['Not found'].append(left_kid[1])
+
+    result = ''
+    for key, value in santa_dict.items():
+        if value:
+            result += f"{key}: {', '.join(value)}\n"
+    return result
+
+
+
+# print(naughty_or_nice_list(
+#     [
+#         (3, "Amy"),
+#         (1, "Tom"),
+#         (7, "George"),
+#         (3, "Katy"),
+#     ],
+#     "3-Nice",
+#     "1-Naughty",
+#     Amy="Nice",
+#     Katy="Naughty",
+# ))
+
+# print(naughty_or_nice_list(
+#     [
+#         (7, "Peter"),
+#         (1, "Lilly"),
+#         (2, "Peter"),
+#         (12, "Peter"),
+#         (3, "Simon"),
+#     ],
+#     "3-Nice",
+#     "5-Naughty",
+#     "2-Nice",
+#     "1-Nice",
+#     ))
 
 print(naughty_or_nice_list(
     [
-        (3, "Amy"),
-        (1, "Tom"),
-        (7, "George"),
-        (3, "Katy"),
+        (6, "John"),
+        (4, "Karen"),
+        (2, "Tim"),
+        (1, "Merry"),
+        (6, "Frank"),
     ],
-    "3-Nice",
+    "6-Nice",
+    "5-Naughty",
+    "4-Nice",
+    "3-Naughty",
+    "2-Nice",
     "1-Naughty",
-    Amy="Nice",
-    Katy="Naughty",
+    Frank="Nice",
+    Merry="Nice",
+    John="Naughty",
 ))
