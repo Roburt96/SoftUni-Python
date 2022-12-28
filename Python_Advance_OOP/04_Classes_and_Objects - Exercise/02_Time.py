854c7c1bdfea452a72c761bef61569493a02f3f5
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2022-12-28 09:41:25 +0200

    procc

---
+++ Python_Advance_OOP/04_Classes_and_Objects - Exercise/02_Time.py
@@ -0,0 +1,18 @@
+class Time:
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours, minutes, seconds):
+        pass
+
+    def get_time(self):
+        pass
+
+    def next_seconds(self):
+        pass
