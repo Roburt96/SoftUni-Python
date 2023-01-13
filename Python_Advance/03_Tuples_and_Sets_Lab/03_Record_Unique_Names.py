27035fce95b51a383d2e6d2694d8870f3eaeb1b8
Roburt96 <roburtsulev96@gmail.com>
2023-01-13 19:05:30 +0200

    done

--- Python_Advance/03_Tuples_and_Sets_Lab/03_Record_Unique_Names.py
+++ Python_Advance/03_Tuples_and_Sets_Lab/03_Record_Unique_Names.py
@@ -1,7 +1,13 @@
-number_of_names = int(input())
-names = set()
-for i in range(number_of_names):
-    name = input()
-    names.add(name)
+# number_of_names = int(input())
+# names = set()
+# for i in range(number_of_names):
+#     name = input()
+#     names.add(name)
+#
+# print('\n'.join(names))
 
-print('\n'.join(names))
+number_of_names = int(input())
+
+names = [input() for i in range(number_of_names)]
+print("\n".join(set(names)))
+
