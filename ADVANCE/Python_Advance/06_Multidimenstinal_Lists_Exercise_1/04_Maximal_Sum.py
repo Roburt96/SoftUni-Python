6d6bdf1d3fc47661a4896c62ed474c236f98bf2c
Roburt96 <roburtsulev96@gmail.com>
2023-01-16 19:34:10 +0200

    100/100

--- ADVANCE/Python_Advance/06_Multidimenstinal_Lists_Exercise_1/04_Maximal_Sum.py
+++ ADVANCE/Python_Advance/06_Multidimenstinal_Lists_Exercise_1/04_Maximal_Sum.py
@@ -1,16 +1,20 @@
 rows, cols = [int(x) for x in input().split()]
 max_sum = -200
-
+best = []
 matrix = [[int(x) for x in input().split()] for i in range(rows)]
 
 
 for row in range(rows):
     for col in range(cols):
         if 0 <= row + 3 <= rows and 0 <= col + 3 <= cols:
+            current_best = []
             current_sum = 0
             for row_check in range(row, row + 3):
+                current_best.append(matrix[row_check][col:col + 3])
                 current_sum += sum(matrix[row_check][col:col + 3])
             if current_sum > max_sum:
                 max_sum = current_sum
+                best = current_best
 
-print(max_sum)
+print(f"Sum = {max_sum}")
+[print(*row) for row in best]
