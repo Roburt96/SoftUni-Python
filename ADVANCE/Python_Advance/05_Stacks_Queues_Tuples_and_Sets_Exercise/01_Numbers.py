1593615d0b4b01dee45d824a807c0a88e9bff1bc
Roburt96 <roburtsulev96@gmail.com>
2023-01-17 19:00:57 +0200

    second sol

--- ADVANCE/Python_Advance/05_Stacks_Queues_Tuples_and_Sets_Exercise/01_Numbers.py
+++ ADVANCE/Python_Advance/05_Stacks_Queues_Tuples_and_Sets_Exercise/01_Numbers.py
@@ -32,3 +32,26 @@ for i in range(n_cmd):
 print(*sorted(row_one_numbers), sep=", ")
 print(*sorted(row_two_numbers), sep=", ")
 
+
+# first = set(int(x) for x in input().split())
+# second = set(int(x) for x in input().split())
+#
+# func = {
+#     "Add First": lambda x: [first.add(el) for el in x],
+#     "Add Second": lambda x: [second.add(el) for el in x],
+#     "Remove First": lambda x: [first.discard(el) for el in x],
+#     "Remove Second": lambda x: [second.add(el) for el in x],
+#     "Check Subset": lambda : print(True) if first.issubset(second) or second.issubset(first) else print(False)
+# }
+#
+# for _ in range(int(input())):
+#     first_command, *data = input().split()
+#     command = first_command + " " + data.pop(0)
+#
+#     if data:
+#         func[command]([int(x) for x in data])
+#     else:
+#         func[command]()
+#
+# print(*sorted(first), sep=", ")
+# print(*sorted(second), sep=", ")
