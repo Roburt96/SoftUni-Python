766d14726a5ebe332e442bacf270b93bebc0a620
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2023-01-31 18:00:43 +0200

    class

--- ADVANCE/Python_Advance_Exams/04_Python Advanced Retake Exam - 14 April 2022/01_Ramen_Shop.py
+++ ADVANCE/Python_Advance_Exams/04_Python Advanced Retake Exam - 14 April 2022/01_Ramen_Shop.py
@@ -1,30 +1,69 @@
 from collections import deque
 
-bowls_of_ramen = deque(int(x) for x in input().split(', '))
-customers = deque(int(x) for x in input().split(', '))
+class RamenShop:
+
+    def __init__(self):
+        self.bowls_of_ramen = deque(int(x) for x in input().split(", "))
+        self.customers = deque(int(x) for x in input().split(", "))
+        self.main()
+
+    def main(self):
+        while self.bowls_of_ramen and self.customers:
+
+            current_ramen = self.bowls_of_ramen.pop()
+            current_customer = self.customers.popleft()
+
+            if current_ramen == current_customer:
+                continue
+
+            if current_ramen > current_customer:
+                current_ramen -= current_customer
+                self.bowls_of_ramen.append(current_ramen)
+
+            elif current_ramen < current_customer:
+                current_customer -= current_ramen
+                self.customers.appendleft(current_customer)
+
+        if len(self.customers) == 0:
+            print(f"Great job! You server all the customers.")
+            if self.bowls_of_ramen:
+                print(f"Bowls of ramen left: {', '.join(str(x) for x in self.bowls_of_ramen)}")
+
+        elif len(self.customers) > 0:
+            print(f"Out of ramen! You didn't manage to serve all customers.")
+            if self.customers:
+                print(f"Customers left: {', '.join(str(x) for x in self.customers)}")
 
 
-while bowls_of_ramen and customers:
-    ramen = bowls_of_ramen.pop()
-    client = customers.popleft()
 
-    if ramen == client:
-        continue
+# bowls_of_ramen = deque(int(x) for x in input().split(', '))
+# customers = deque(int(x) for x in input().split(', '))
+#
+#
+# while bowls_of_ramen and customers:
+#     ramen = bowls_of_ramen.pop()
+#     client = customers.popleft()
+#
+#     if ramen == client:
+#         continue
+#
+#     if ramen > client:
+#         ramen -= client
+#         bowls_of_ramen.append(ramen)
+#
+#     elif ramen < client:
+#         client -= ramen
+#         customers.appendleft(client)
+#
+# if len(customers) == 0:
+#     print(f"Great job! You served all the customers.")
+#     if bowls_of_ramen:
+#         print(f"Bowls of ramen left: {', '.join(str(x) for x in bowls_of_ramen)}")
+#
+# elif len(customers) > 0:
+#     print(f"Out of ramen! You didn't manage to serve all customers.")
+#     if customers:
+#         print(f"Customers left: {', '.join(str(x) for x in customers)}")
 
-    if ramen > client:
-        ramen -= client
-        bowls_of_ramen.append(ramen)
-
-    elif ramen < client:
-        client -= ramen
-        customers.appendleft(client)
-
-if len(customers) == 0:
-    print(f"Great job! You served all the customers.")
-    if bowls_of_ramen:
-        print(f"Bowls of ramen left: {', '.join(str(x) for x in bowls_of_ramen)}")
-
-elif len(customers) > 0:
-    print(f"Out of ramen! You didn't manage to serve all customers.")
-    if customers:
-        print(f"Customers left: {', '.join(str(x) for x in customers)}")
+if __name__ == '__main__':
+    RamenShop()
--- ADVANCE/Python_Advance_Exams/05_Python Advanced Exam - 19 February 2022/01_Flowers_Findeers.py
+++ ADVANCE/Python_Advance_Exams/05_Python Advanced Exam - 19 February 2022/01_Flowers_Findeers.py
@@ -1,34 +1,74 @@
 from collections import deque
-vowels = deque(input().split())
-consonants = deque(input().split())
-flowers = {
-    "rose": "rose",
-    "tulip": "tulip",
-    "lotus": "lotus",
-    "daffodil": "daffodil"
-}
 
-find = False
-word = ""
+class Flowers:
 
-while vowels and consonants and not find:
-    first_ch = vowels.popleft()
-    second_ch = consonants.pop()
-    for key, value in flowers.items():
-        if first_ch in value:
-            flowers[key] = flowers[key].replace(first_ch, "")
-        if second_ch in value:
-            flowers[key] = flowers[key].replace(second_ch, "")
-        if not flowers[key]:
-            word = key
-            find = True
-            break
+    def __init__(self):
+        self.vowels = deque(input().split())
+        self.consonants = deque(input().split())
+        self.flowers = {
+            "rose": "rose",
+            "tulip": "tulip",
+            "lotus": "lotus",
+            "daffodil": "daffodil"
+        }
+        self.find = False
+        self.word = ""
+        self.main()
 
-if find:
-    print(f"Word found: {word}")
-elif not find:
-    print("Cannot find any word!")
-if vowels:
-    print(f"Vowels left: {' '.join(vowels)}")
-if consonants:
-    print(f"Consonants left: {' '.join(consonants)}")
+    def main(self):
+        while self.vowels and self.consonants and not self.find:
+            first_character = self.vowels.popleft()
+            second_character = self.consonants.pop()
+            for key, value in self.flowers.items():
+                if first_character in value:
+                    self.flowers[key] = self.flowers[key].replace(first_character, "")
+                if second_character in value:
+                    self.flowers[key] = self.flowers[key].replace(second_character, "")
+                if not self.flowers[key]:
+                    self.word = key
+                    self.find = True
+                    break
+        if self.find:
+            print(f"Word found: {self.word}")
+        elif not self.find:
+            print("Cannot find any word!")
+        if self.vowels:
+            print(f"Vowels left: {' '.join(self.vowels)}")
+        if self.consonants:
+            print(f"Consonants left: {' '.join(self.consonants)}")
+
+if __name__ == '__main__':
+    Flowers()
+# vowels = deque(input().split())
+# consonants = deque(input().split())
+# flowers = {
+#     "rose": "rose",
+#     "tulip": "tulip",
+#     "lotus": "lotus",
+#     "daffodil": "daffodil"
+# }
+#
+# find = False
+# word = ""
+#
+# while vowels and consonants and not find:
+#     first_ch = vowels.popleft()
+#     second_ch = consonants.pop()
+#     for key, value in flowers.items():
+#         if first_ch in value:
+#             flowers[key] = flowers[key].replace(first_ch, "")
+#         if second_ch in value:
+#             flowers[key] = flowers[key].replace(second_ch, "")
+#         if not flowers[key]:
+#             word = key
+#             find = True
+#             break
+#
+# if find:
+#     print(f"Word found: {word}")
+# elif not find:
+#     print("Cannot find any word!")
+# if vowels:
+#     print(f"Vowels left: {' '.join(vowels)}")
+# if consonants:
+#     print(f"Consonants left: {' '.join(consonants)}")
