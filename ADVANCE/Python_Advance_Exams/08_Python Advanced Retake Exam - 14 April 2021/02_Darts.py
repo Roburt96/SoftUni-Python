d6963471c363f15e368531097a9930efb4f175c0
Roburt96 <110181057+Roburt96@users.noreply.github.com>
2023-02-13 13:56:50 +0200

    .

--- ADVANCE/Python_Advance_Exams/08_Python Advanced Retake Exam - 14 April 2021/02_Darts.py
+++ ADVANCE/Python_Advance_Exams/08_Python Advanced Retake Exam - 14 April 2021/02_Darts.py
@@ -1,41 +1,45 @@
-rows, cols, names, score_board = 7, 7, input().split(", "), {}
+# rows, cols, names, score_board = 7, 7, input().split(", "), {}
+#
+# for i in range(len(names)):
+#     score_board[names[i]] = score_board.get(names[i], {'score': 501, 'throw': 0})
+#
+# matrix = [[int(x) if x.isdigit() else x for x in input().split()] for _ in range(rows)]
+#
+#
+# while True:
+#     row, col = [int(x) for x in input()[1:-1].split(", ")]
+#     player = names[0]
+#     score_board[player]['throw'] += 1
+#
+#     if row >= len(matrix) or col >= len(matrix):
+#         names.append(names.pop(0))
+#         continue
+#
+#     if matrix[row][col] == "B":
+#         print(f"{player} won the game with {score_board[player]['throw']} throws!")
+#         break
+#
+#     elif matrix[row][col] == 'D':
+#         total_sum = (matrix[row][0] + matrix[row][6] + matrix[0][col] + matrix[6][col]) * 2
+#         score_board[player]['score'] -= total_sum
+#
+#
+#     elif matrix[row][col] == 'T':
+#         total_sum = (matrix[row][0] + matrix[row][6] + matrix[0][col] + matrix[6][col]) * 3
+#         score_board[player]['score'] -= total_sum
+#
+#
+#     else:
+#         score_board[player]['score'] -= matrix[row][col]
+#
+#     if score_board[player]['score'] <= 0:
+#         print(f"{player} won the game with {score_board[player]['throw']} throws!")
+#         break
+#     names.append(names.pop(0))
 
-for i in range(len(names)):
-    score_board[names[i]] = score_board.get(names[i], {'score': 501, 'throw': 0})
+class Darts:
 
-matrix = [[int(x) if x.isdigit() else x for x in input().split()] for _ in range(rows)]
-
-
-while True:
-    row, col = [int(x) for x in input()[1:-1].split(", ")]
-    player = names[0]
-    score_board[player]['throw'] += 1
-
-    if row >= len(matrix) or col >= len(matrix):
-        names.append(names.pop(0))
-        continue
-
-    if matrix[row][col] == "B":
-        print(f"{player} won the game with {score_board[player]['throw']} throws!")
-        break
-
-    elif matrix[row][col] == 'D':
-        total_sum = (matrix[row][0] + matrix[row][6] + matrix[0][col] + matrix[6][col]) * 2
-        score_board[player]['score'] -= total_sum
-
-
-    elif matrix[row][col] == 'T':
-        total_sum = (matrix[row][0] + matrix[row][6] + matrix[0][col] + matrix[6][col]) * 3
-        score_board[player]['score'] -= total_sum
-
-
-    else:
-        score_board[player]['score'] -= matrix[row][col]
-
-    if score_board[player]['score'] <= 0:
-        print(f"{player} won the game with {score_board[player]['throw']} throws!")
-        break
-    names.append(names.pop(0))
+    def __init__(self):
 
 
 
--- ADVANCE/Python_Advance_Exams/09_Python Advanced Exam - 14 February 2021/02_Collecting_Coins.py
+++ ADVANCE/Python_Advance_Exams/09_Python Advanced Exam - 14 February 2021/02_Collecting_Coins.py
@@ -1,59 +1,134 @@
-move = {
-    'left': {'row': 0, 'col': -1},
-    'right': {'row': 0, 'col': 1},
-    'up': {'row': -1, 'col': 0},
-    'down': {'row': 1, 'col': 0}
-}
+# move = {
+#     'left': {'row': 0, 'col': -1},
+#     'right': {'row': 0, 'col': 1},
+#     'up': {'row': -1, 'col': 0},
+#     'down': {'row': 1, 'col': 0}
+# }
+#
+# size = int(input())
+# p_row, p_col = 0, 0
+# matrix = []
+# coins = 0
+# path  = []
+# win = False
+#
+# for row in range(size):
+#     matrix.append([int(x) if x.isdigit() else x for x in input().split()])
+#     if "P" in matrix[row]:
+#         p_row, p_col = row, matrix[row].index('P')
+#         path.append([p_row, p_col])
+#         matrix[p_row][p_col] = '.'
+#
+#
+# while True:
+#     move_command = input()
+#
+#     if move[move_command]:
+#         p_row, p_col = p_row + move[move_command]['row'], p_col + move[move_command]['col']
+#
+#         if p_row < 0:
+#             p_row = size - 1
+#         if p_row > size - 1:
+#             p_row = 0
+#         if p_col < 0:
+#             p_col = size - 1
+#         if p_col > size - 1:
+#             p_col = 0
+#         path.append([p_row, p_col])
+#         if matrix[p_row][p_col] == "X":
+#             coins = round(coins * 0.5)
+#             break
+#
+#         elif matrix[p_row][p_col] == ".":
+#             continue
+#
+#         else:
+#             coins += matrix[p_row][p_col]
+#             matrix[p_row][p_col] = "."
+#
+#     if coins >= 100:
+#         win = True
+#         break
+#
+#
 
-size = int(input())
-p_row, p_col = 0, 0
-matrix = []
-coins = 0
-path  = []
-win = False
 
-for row in range(size):
-    matrix.append([int(x) if x.isdigit() else x for x in input().split()])
-    if "P" in matrix[row]:
-        p_row, p_col = row, matrix[row].index('P')
-        path.append([p_row, p_col])
-        matrix[p_row][p_col] = '.'
+class CollectingCoins:
 
+    def __init__(self):
+        self.size = int(input())
+        self.matrix = []
+        self.row = 0
+        self.col = 0
+        self.collected_coins = 0
+        self.path = []
+        self.win = False
+        self.main()
 
-while True:
-    move_command = input()
+    def main(self):
+        self.make_matrix()
+        self.main_loop()
 
-    if move[move_command]:
-        p_row, p_col = p_row + move[move_command]['row'], p_col + move[move_command]['col']
+    def make_matrix(self):
+        for row in range(self.size):
+            self.matrix.append(int(x) if x.isdigit() else x for x in input().split(','))
+            if 'P' in self.matrix[row]:
+                self.row, self.col = row, self.matrix[row].index('P')
+                self.path.append([self.row, self.col])
+                self.matrix[self.row][self.col] = '.'
 
-        if p_row < 0:
-            p_row = size - 1
-        if p_row > size - 1:
-            p_row = 0
-        if p_col < 0:
-            p_col = size - 1
-        if p_col > size - 1:
-            p_col = 0
-        path.append([p_row, p_col])
-        if matrix[p_row][p_col] == "X":
-            coins = round(coins * 0.5)
-            break
+    def main_loop(self):
+        while True:
+            cur_move = input()
 
-        elif matrix[p_row][p_col] == ".":
-            continue
+            if cur_move == 'up':
+                self.row -= 1
 
+            elif cur_move == 'down':
+                self.row += 1
+
+            elif cur_move == 'left':
+                self.col -= 1
+
+            elif cur_move == 'right':
+                self.col += 1
+
+            if self.row >= self.size:
+                self.row = 0
+            elif self.row < 0 :
+                self.row = self.size - 1
+
+            if self.col >= self.size:
+                self.col = 0
+            elif self.col < 0 :
+                self.col = self.size - 1
+
+            if self.matrix[self.row][self.col] == 'X':
+                self.collected_coins /= 2
+                break
+
+            elif self.matrix[self.row][self.col] == ".":
+                continue
+
+            else:
+                self.collected_coins += self.matrix[self.row][self.col]
+                self.path.append([self.row, self.col])
+                self.matrix[self.row][self.col] = '.'
+
+            if self.collected_coins >= 100:
+                self.win = True
+                break
+
+        if not self.win:
+            print(f"Game over! You've collected {round(self.collected_coins)} coins.")
         else:
-            coins += matrix[p_row][p_col]
-            matrix[p_row][p_col] = "."
-
-    if coins >= 100:
-        win = True
-        break
+            print(f"You won! You've collected {round(self.collected_coins)} coins.")
+        print('Your path:')
+        print('\n'.join(str(x) for x in self.path))
 
 
-if not win:
-    print(f"Game over! You've collected {coins} coins.")
-else:
-    print(f"You won! You've collected {coins} coins.")
-print('Your path:')
-print('\n'.join(str(x) for x in path))
+if __name__ == '__main__':
+    CollectingCoins()
+
+
+
