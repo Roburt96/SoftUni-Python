49710066c565a2c5f270f5759742d9759f278320
Roburt96 <roburtsulev96@gmail.com>
2023-03-25 15:56:03 +0200

    done

---
+++ 03_Advance/Python_OOP/09_Decorators/Decorators - Exercise/Type_Checker.py
@@ -0,0 +1,24 @@
+def type_check(type):
+    def decorator(func):
+        def wrapper(*args):
+            if not isinstance(*args, type):
+                return "Bad Type"
+            return func(*args)
+
+        return wrapper
+
+    return decorator
+
+
+@type_check(int)
+def times2(num):
+    return num * 2
+
+
+print(times2(2))
+print(times2('Not A Number'))
+
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
